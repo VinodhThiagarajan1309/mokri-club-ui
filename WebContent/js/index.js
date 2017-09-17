8ba4ef1393c25124fa6c5f002efe2b2eb2768167
vthiagarajan <vthiagarajan@homeaway.com>
2017-09-17 14:59:54 -0500

    Calling Mpeg attachments

---
+++ WebContent/js/index.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+	initFileUploader("#zdrop");
+		function initFileUploader(target) {
+			var previewNode = document.querySelector("#zdrop-template");
+			previewNode.id = "";
+			var previewTemplate = previewNode.parentNode.innerHTML;
+			previewNode.parentNode.removeChild(previewNode);
+
+
+			var zdrop = new Dropzone(target, {
+				url: 'https://vinodh.adaptainer.io/tryMpeg/upload/',
+				acceptedFiles: ".mp3",
+                maxFiles:1,
+				maxFilesize:10,
+				previewTemplate: previewTemplate,
+				previewsContainer: "#previews",
+				clickable: "#upload-label",
+				success: function(file, response) {
+					var responsetext = JSON.parse(file.xhr.responseText);
+					console.log(responsetext);
+				}
+			});
+
+			zdrop.on("addedfile", function(file) { 
+				$('.preview-container').css('visibility', 'visible');
+			});
+
+			zdrop.on("totaluploadprogress", function (progress) {
+				var progr = document.querySelector(".progress .determinate");
+					if (progr === undefined || progr === null)
+							return;
+
+						progr.style.width = progress + "%";
+					});
+
+					zdrop.on('dragenter', function () {
+						$('.fileuploader').addClass("active");
+					});
+
+					zdrop.on('dragleave', function () {
+						$('.fileuploader').removeClass("active");			
+					});
+
+					zdrop.on('drop', function () {
+						$('.fileuploader').removeClass("active");	
+					});
+					
+		  }
+
+	});
